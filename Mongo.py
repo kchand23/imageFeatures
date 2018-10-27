557e5c6821e301602a445bc78d071aad5252fbc7
Lorenzo Semeria <lorenzo.semeria@mail.polimi.it>
2018-10-27 17:52:43 -0500

    Added stfout flush to make bars/dot appear timely

--- Mongo.py
+++ Mongo.py
@@ -4,7 +4,7 @@
 '''
 '''
   Edited by Lorenzo Semeria (lor.semeria@gmail.com)
-  Fall 2018 
+  Fall 2018
 '''
 try:
     import wildbook
@@ -17,16 +17,17 @@ try:
 except ImportError:
     from imageFeatures import beautyFtr as beauty
 import pymongo
+import sys
 
 WildbookAPI = wildbook.WildbookAPI
 ''' Make sure you have PyMongo installed via pip. '''
 from pymongo import MongoClient as mongo
 
 DB_URL = 'mongodb://localhost:27017/'                               ## MongoDB
-SERVER_URL =  'http://71.59.132.88:5009'    ## GGR!!! NON GGR2                   ## IBEIS Server (pachy or other) 'http://pachy.cs.uic.edu:5001'
+SERVER_URL =  'http://pachy.cs.uic.edu:5001'             ## IBEIS Server (pachy or other) 'http://pachy.cs.uic.edu:5001'
 IMAGES_TO_ANALYZE = 2                                            ## How many images to analyze
 RANDOM_GIDS = False                                                 ## Should GIDs (images) be picked randomly?
-DB_NAME = 'tesiWildbook2018-GGR2'                                                ## Name of database in MongoDB.
+DB_NAME = 'tesiWildbook2018-realGGR2'                                                ## Name of database in MongoDB.
 COLLECTION_NAME = 'images'                                          ## Name of collection in MongoDB.
 path_join = os.path.join                                            ## Shorthand function
 
@@ -97,6 +98,7 @@ def store_image_samples(destination_dir, api, gid_list_in=None):
   for t,gid in enumerate(gid_list[:number_images_to_analyze]):
     if t%1000 == 0:
         print("Downloading image", t, "of", number_images_to_analyze)
+    sys.stdout.flush()
     if gid in image_names_list:
       #print(gid, ' appears to be here already')
       print('.', end='')
