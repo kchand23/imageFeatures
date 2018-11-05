2f078ef2a762cea951e7595586316d01507b8b66
Lorenzo Semeria <lorenzo.semeria@mail.polimi.it>
2018-11-04 22:44:09 -0600

    added little output to track progress

--- Mongo.py
+++ Mongo.py
@@ -172,7 +172,10 @@ def main(db_url=DB_URL, server_url=SERVER_URL, db_name=DB_NAME, collection_name=
   ''' Iteratively store image properties to MongoDB. '''
   if redo_beauty:
       image_list = os.listdir(destination_dir)
-  for image in image_list:
+
+  for i, image in enumerate(image_list):
+    if i%1000 == 0:
+        print('Image', i, 'of', len(image_list)
     gid = str(image).replace('.jpg','')
     aid_list = api.get_aid_of_gid(gid)[0]
     info = os.stat(path_join(destination_dir , image))
